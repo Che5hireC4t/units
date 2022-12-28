582027757745a8ec15c6a5760f669f2d8bd4f26b
Che5hireC4t <matthieu.epiard@protonmail.com>
2022-12-28 18:46:40 +0100

    Adding multiplication data to conftest.

--- unittests/Test_Dimensions_Module/conftest.py
+++ unittests/Test_Dimensions_Module/conftest.py
@@ -14,3 +14,13 @@ CONVERSION_TABLES_INIT_DATA =  \
         {'test_dimension': Acceleration, 'length_exponent': 1, 'time_exponent': -2},
         {'test_dimension': Pressure, 'mass_exponent': 1, 'length_exponent': -1, 'time_exponent': -2}
     )
+
+
+
+MULTIPLICATION_DATA = \
+    {
+        (Length, Length): Surface,
+        (Length, Surface): Volume,
+        (ElectricCurrent, Time): ElectricCharge,
+        (Length, Frequency): Speed
+    }
