159f887dcacd2305f0ed344b97e164c79f7670e1
Che5hireC4t <matthieu.epiard@protonmail.com>
2022-12-28 17:36:34 +0100

    Tests optimization by using the conversion tables.

--- unittests/Test_Dimensions_Module/test_correctly_create_quantities.py
+++ unittests/Test_Dimensions_Module/test_correctly_create_quantities.py
@@ -1,40 +1,44 @@
 import pytest
 from math import isnan
-from unittests.Fuzzer import Fuzzer
-from unittests.Test_Dimensions_Module.__symbols_generators import generate_symbols
+
+from unittests.Test_Dimensions_Module.dependencies import ConversionArray
+from unittests.Test_Dimensions_Module.conftest import CONVERSION_TABLES_INIT_DATA
 
 
-@pytest.mark.parametrize\
-        (
-            'symbol, dimension_class',
-            (symbol_class_tuple for symbol_class_tuple in generate_symbols())
-        )
-def test_symbols(symbol: str, dimension_class):
-    quantity = dimension_class(1.0, symbol)
-    assert isinstance(quantity, dimension_class)
+@pytest.mark.parametrize('conversion_table_init_data', CONVERSION_TABLES_INIT_DATA)
+def test_symbols(conversion_table_init_data):
+    conversion_table = ConversionArray(**conversion_table_init_data)
+    dimension_class = conversion_table.dimension_class
+    yield_random = True if conversion_table.size > 1000 else False
+    for _, symbol, _, _ in conversion_table.generate_test_data(yield_random):
+        quantity = dimension_class(1.0, symbol)
+        assert isinstance(quantity, dimension_class)
     return
 
 
-fuzzer = Fuzzer()
-@pytest.mark.parametrize\
-        (
-            'symbol, dimension_class, random_integer',
-            ([*symbol_class_tuple, fuzzer.logarithmic_randint()] for symbol_class_tuple in generate_symbols())
-        )
-def test_instantiate_by_int(symbol: str, dimension_class, random_integer: int):
-    quantity = dimension_class(random_integer, symbol)
-    assert isinstance(quantity, dimension_class)
-    assert float(quantity) == float(random_integer)
+
+@pytest.mark.parametrize('conversion_table_init_data', CONVERSION_TABLES_INIT_DATA)
+def test_instantiate_by_int(conversion_table_init_data, supply_fuzzer):
+    conversion_table = ConversionArray(**conversion_table_init_data)
+    dimension_class = conversion_table.dimension_class
+    yield_random = True if conversion_table.size > 1000 else False
+    random_integer = supply_fuzzer.logarithmic_randint()
+    for _, symbol, _, _ in conversion_table.generate_test_data(yield_random):
+        quantity = dimension_class(random_integer, symbol)
+        assert isinstance(quantity, dimension_class)
+        assert float(quantity) == float(random_integer)
     return
 
 
-@pytest.mark.parametrize\
-        (
-            'symbol, dimension_class, random_float',
-            ([*symbol_class_tuple, fuzzer.logarithmic_randfloat()] for symbol_class_tuple in generate_symbols())
-        )
-def test_instantiate_by_float(symbol: str, dimension_class, random_float: float):
-    quantity = dimension_class(random_float, symbol)
-    assert isinstance(quantity, dimension_class)
-    assert float(quantity) == random_float or (isnan(float(quantity)) and isnan(random_float))
+
+@pytest.mark.parametrize('conversion_table_init_data', CONVERSION_TABLES_INIT_DATA)
+def test_instantiate_by_float(conversion_table_init_data, supply_fuzzer):
+    conversion_table = ConversionArray(**conversion_table_init_data)
+    dimension_class = conversion_table.dimension_class
+    yield_random = True if conversion_table.size > 1000 else False
+    random_float = supply_fuzzer.logarithmic_randfloat()
+    for _, symbol, _, _ in conversion_table.generate_test_data(yield_random):
+        quantity = dimension_class(random_float, symbol)
+        assert isinstance(quantity, dimension_class)
+        assert float(quantity) == random_float or (isnan(float(quantity)) and isnan(random_float))
     return
