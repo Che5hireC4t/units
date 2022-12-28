3f4ebd389b53d7a68f021d3c0e2f9ba6004abedb
Che5hireC4t <matthieu.epiard@protonmail.com>
2022-12-28 19:27:30 +0100

    Creating a test for multiplications.

--- unittests/Test_Dimensions_Module/test_multiplication.py
+++ unittests/Test_Dimensions_Module/test_multiplication.py
@@ -1,19 +1,21 @@
 import pytest
-from dimensions import *
+from unittests.Test_Dimensions_Module.dependencies import ConversionArray
+from unittests.Test_Dimensions_Module.conftest import MULTIPLICATION_DATA, CONVERSION_TABLES_INIT_DATA
 
 
-MULTIPLICATION_DATA = \
-    {
-        (Length, Length): Surface,
-        (Length, Surface): Volume,
-        (ElectricCurrent, Time): ElectricCharge,
-        (Length, Frequency): Speed
-    }
 
-
-def test_double_multiplication(multiplier_1: tuple, multiplier_2: tuple, expected_type):
-    number_1, symbol_1, dim_class_1 = multiplier_1
-    number_2, symbol_2, dim_class_2 = multiplier_2
-    quantity_1 = dim_class_1(number_1, symbol_1)
-    quantity_2 = dim_class_2(number_2, symbol_2)
-    final_result = quantity_1 * quantity_2
+@pytest.mark.parametrize('dimension_name1, dimension_name2, expected_type', MULTIPLICATION_DATA)
+def test_yield_correct_dimension(dimension_name1: str, dimension_name2: str, expected_type):
+    conversion_table1 = ConversionArray(**CONVERSION_TABLES_INIT_DATA[dimension_name1])
+    conversion_table2 = ConversionArray(**CONVERSION_TABLES_INIT_DATA[dimension_name2])
+    yield_random1 = True if conversion_table1.size > 1000 else False
+    yield_random2 = True if conversion_table2.size > 1000 else False
+    dimension_class_1 = conversion_table1.dimension_class
+    dimension_class_2 = conversion_table2.dimension_class
+    for test_number1, symbol1 in conversion_table1.generate_test_values(yield_random1):
+        for test_number2, symbol2 in conversion_table2.generate_test_values(yield_random2):
+            quantity1 = dimension_class_1(test_number1, symbol1)
+            quantity2 = dimension_class_2(test_number2, symbol2)
+            multiplied_quantity = quantity1 * quantity2
+            assert isinstance(multiplied_quantity, expected_type)
+    return
