6ce3d11541d56fb961611d490e304f9ef3674471
Che5hireC4t <matthieu.epiard@protonmail.com>
2022-12-28 12:25:01 +0100

    Finalizing the test of conversion functionality.

---
+++ unittests/Test_Dimensions_Module/conftest.py
@@ -0,0 +1,16 @@
+from dimensions import *
+
+
+CONVERSION_TABLES_INIT_DATA =  \
+    (
+        {'test_dimension': Length, 'length_exponent': 1},
+        {'test_dimension': Mass, 'mass_exponent': 1},
+        {'test_dimension': Time, 'time_exponent': 1},
+        {'test_dimension': SubstanceAmount, 'substance_amount_exponent': 1},
+        {'test_dimension': Surface, 'length_exponent': 2},
+        {'test_dimension': Volume, 'length_exponent': 3},
+        {'test_dimension': Speed, 'length_exponent': 1, 'time_exponent': -1},
+        {'test_dimension': Frequency, 'time_exponent': -1},
+        {'test_dimension': Acceleration, 'length_exponent': 1, 'time_exponent': -2},
+        {'test_dimension': Pressure, 'mass_exponent': 1, 'length_exponent': -1, 'time_exponent': -2}
+    )
--- unittests/Test_Dimensions_Module/test_conversion.py
+++ unittests/Test_Dimensions_Module/test_conversion.py
@@ -1,38 +1,19 @@
 import pytest
 from math import isclose
-from dimensions import *
-from unittests.Test_Dimensions_Module.ConversionArray import ConversionArray
+
+from unittests.Test_Dimensions_Module.dependencies import ConversionArray
+from unittests.Test_Dimensions_Module.conftest import CONVERSION_TABLES_INIT_DATA
 
 
 
-CONVERSION_TABLES = \
-    (
-        ConversionArray(Pressure, mass_exponent=1, length_exponent=-1, time_exponent=-2),
-        ConversionArray(Length, length_exponent=1),
-        # ConversionArray(Mass, mass_exponent=1),
-        # ConversionArray(Time, time_exponent=1),
-        # ConversionArray(SubstanceAmount, substance_amount_exponent=1),
-        # ConversionArray(Surface, length_exponent=2),
-        # ConversionArray(Volume, length_exponent=3),
-        # ConversionArray(Speed, length_exponent=1, time_exponent=-1),
-        # ConversionArray(Frequency, time_exponent=-1),
-        # ConversionArray(Acceleration, length_exponent=1, time_exponent=-2),
-
-    )
-
-
-
-@pytest.mark.parametrize('conversion_table', CONVERSION_TABLES)
-def test_convert(conversion_table: ConversionArray):
+@pytest.mark.parametrize('conversion_table_init_data', CONVERSION_TABLES_INIT_DATA)
+def test_convert(conversion_table_init_data: dict):
+    conversion_table = ConversionArray(**conversion_table_init_data)
     dimension_class = conversion_table.dimension_class
     yield_random = True if conversion_table.size > 1000 else False
-    # generator = list(conversion_table.generate_test_data(yield_random))
-    counter = 0
     for test_number, source_symbol, target_symbol, expected_number in conversion_table.generate_test_data(yield_random):
-        counter += 1
         test_quantity = dimension_class(test_number, source_symbol)
         converted_quantity = test_quantity.convert(target_symbol)
-        x = 0
         assert isclose(float(converted_quantity), expected_number, rel_tol=1e-4),\
             f"Failed to convert {dimension_class.__name__} quantity {test_quantity} in {target_symbol}."
     return
