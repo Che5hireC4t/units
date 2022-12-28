abddfd23d355d83b60a8eb67c4112d7c31844d4d
Che5hireC4t <matthieu.epiard@protonmail.com>
2022-12-28 11:55:04 +0100

    Changing conversions constant because the international unit of mass is the kilogram and not the gram.

--- dimensions/Mass.py
+++ dimensions/Mass.py
@@ -6,8 +6,8 @@ class Mass(AbstractQuantity):
     _DIMENSIONAL_ARRAY = DimensionalArray(mass_exponent=1)
     _UNITS = \
         {
-            Unit('g', 'gram', 1.0): 1.0,
-            Unit('oz', 'ounce', 28.349523125): 28.349523125,
-            Unit('lb', 'pound', 453.59237): 453.59237,
-            Unit('t', 'ton', 1000000.0): 1000000.0
+            Unit('g', 'gram', 0.001): 0.001,
+            Unit('oz', 'ounce', 0.028349523125): 0.028349523125,
+            Unit('lb', 'pound', 0.45359237): 0.45359237,
+            Unit('t', 'ton', 1000.0): 1000.0
         }
