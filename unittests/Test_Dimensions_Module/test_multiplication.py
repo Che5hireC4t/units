d6c1eb753acfe044bb46d515139eb8ecf255038b
Che5hireC4t <matthieu.epiard@protonmail.com>
2022-12-28 19:51:00 +0100

    New test to check the multiplication by an adimensional number.

--- unittests/Test_Dimensions_Module/test_multiplication.py
+++ unittests/Test_Dimensions_Module/test_multiplication.py
@@ -1,4 +1,6 @@
 import pytest
+from math import isnan
+
 from unittests.Test_Dimensions_Module.dependencies import ConversionArray
 from unittests.Test_Dimensions_Module.conftest import MULTIPLICATION_DATA, CONVERSION_TABLES_INIT_DATA
 
@@ -18,4 +20,27 @@ def test_yield_correct_dimension(dimension_name1: str, dimension_name2: str, exp
             quantity2 = dimension_class_2(test_number2, symbol2)
             multiplied_quantity = quantity1 * quantity2
             assert isinstance(multiplied_quantity, expected_type)
+    return
+
+
+
+@pytest.mark.parametrize('conversion_table_init_data', CONVERSION_TABLES_INIT_DATA.values())
+def test_multiply_by_adimensional(conversion_table_init_data, supply_fuzzer):
+    conversion_table = ConversionArray(**conversion_table_init_data)
+    dimension_class = conversion_table.dimension_class
+    yield_random = True if conversion_table.size > 1000 else False
+    for test_number, symbol in conversion_table.generate_test_values(yield_random):
+        test_quantity = dimension_class(test_number, symbol)
+        if bool(supply_fuzzer.randint(0, 1)):
+            random_number = supply_fuzzer.logarithmic_randint()
+        else:
+            random_number = supply_fuzzer.logarithmic_randfloat()
+        expected_number = test_number * random_number
+        final_quantity = test_quantity * random_number
+        assert float(final_quantity) == expected_number or (isnan(float(final_quantity)) and isnan(expected_number)),\
+            f"Multiplication by adimensional number gave an unexpected result.\n" \
+            f"{repr(test_quantity)} x {random_number} --> {repr(test_quantity)}"
+        assert isinstance(final_quantity, dimension_class),\
+            f"Multiplication by adimensional number has changed the dimension:\n" \
+            f"{type(test_quantity)} x {random_number} --> {type(test_quantity)}"
     return
