7590d428b9c1986d573ddf97d18782e31cba6260
Che5hireC4t <matthieu.epiard@protonmail.com>
2022-12-29 17:47:05 +0100

    Bugfix in test caused by the use of a wrong variable.

--- unittests/Test_Dimensions_Module/test_conversion.py
+++ unittests/Test_Dimensions_Module/test_conversion.py
@@ -9,7 +9,7 @@ from unittests.Test_Dimensions_Module.conftest import CONVERSION_TABLES_INIT_DAT
 @pytest.mark.parametrize('conversion_table_init_data', CONVERSION_TABLES_INIT_DATA.values())
 def test_convert(conversion_table_init_data: dict):
     dimension_class, conversion_values_generator = get_conversion_data_generator(**conversion_table_init_data)
-    for test_number, source_symbol, target_symbol, expected_number in dimension_class:
+    for test_number, source_symbol, target_symbol, expected_number in conversion_values_generator:
         test_quantity = dimension_class(test_number, source_symbol)
         converted_quantity = test_quantity.convert(target_symbol)
         assert isclose(float(converted_quantity), expected_number, rel_tol=1e-4),\
