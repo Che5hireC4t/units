03299e456181aafdde8cf820991f45689d49fe57
Che5hireC4t <matthieu.epiard@protonmail.com>
2022-12-29 19:13:48 +0100

    Fine tuning conversion constant.

--- dimensions/Energy.py
+++ dimensions/Energy.py
@@ -8,7 +8,7 @@ class Energy(AbstractQuantity):
     _UNITS = \
         {
             Unit('J', 'joule', 1.0): 1.0,
-            Unit('cal', 'calorie', 4.18): 4.18,
+            Unit('cal', 'calorie', 4.184): 4.184,
             Unit('eV', 'electron-volt', 1.60217733e-19): 1.60217733e-19,
             Unit('toe', 'ton-oil-equivalent', 41.868e9): 41.868e9
         }
