a51cdc7ba0671edb68ae6d1f132fb546099e3b0b
Che5hireC4t <matthieu.epiard@protonmail.com>
2022-12-29 19:17:54 +0100

    Fine tuning conversion constant (electron-volt).

--- dimensions/Energy.py
+++ dimensions/Energy.py
@@ -9,7 +9,7 @@ class Energy(AbstractQuantity):
         {
             Unit('J', 'joule', 1.0): 1.0,
             Unit('cal', 'calorie', 4.184): 4.184,
-            Unit('eV', 'electron-volt', 1.60217733e-19): 1.60217733e-19,
+            Unit('eV', 'electron-volt', 1.602176634e-19): 1.602176634e-19,
             Unit('toe', 'ton-oil-equivalent', 41.868e9): 41.868e9
         }
 
