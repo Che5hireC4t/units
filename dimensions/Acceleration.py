c5947037599de6006319826bcebbcecb6033d805
Che5hireC4t <matthieu.epiard@protonmail.com>
2022-12-29 18:42:39 +0100

    Fixing bad conversion constant for acceleration.

--- dimensions/Acceleration.py
+++ dimensions/Acceleration.py
@@ -4,4 +4,4 @@ from dimensions.AbstractQuantity import AbstractQuantity, DimensionalArray, Unit
 class Acceleration(AbstractQuantity):
 
     _DIMENSIONAL_ARRAY = DimensionalArray(length_exponent=1, time_exponent=-2)
-    _UNITS = {Unit('g', 'gravity', 1.0): 1.0}
+    _UNITS = {Unit('g', 'gravity', 9.80665): 9.80665}
