c4e8a2c714882859a6eb8dea5340b45376f59d15
Che5hireC4t <matthieu.epiard@protonmail.com>
2022-12-30 14:50:35 +0100

    Restoring OACI_NORMALIZED_ATMOSPHERE_TEMPERATURE_GRADIENT.

--- constants/geological.py
+++ constants/geological.py
@@ -7,7 +7,7 @@ POLAR_EARTH_RADIUS = Length(6356752, 'm')
 G_FORCE = Acceleration(9.80665, 'm sec-2')
 
 # https://fr.wikipedia.org/wiki/Atmosph%C3%A8re_normalis%C3%A9e#Atmosph%C3%A8re_type_OACI
-# __temperature_gradient_class = Temperature / Length
-# OACI_NORMALIZED_ATMOSPHERE_TEMPERATURE_GRADIENT = __temperature_gradient_class(0.0065, 'K m-1')
+__temperature_gradient_class = Temperature / Length
+OACI_NORMALIZED_ATMOSPHERE_TEMPERATURE_GRADIENT = __temperature_gradient_class(0.0065, 'K m-1')
 PRESSURE_AT_SEA_LEVEL = Pressure(101325, 'Pa')
 TEMPERATURE_AT_SEA_LEVEL = Temperature(288.15, 'K')
