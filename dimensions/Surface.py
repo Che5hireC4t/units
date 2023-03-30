13fb1bfb77037188110dc9c67056d39d333a6514
Che5hireC4t <matthieu.epiard@protonmail.com>
2023-03-30 15:48:50 -0400

    Overloading __pow__ for Surface, allowing to pass 0.5 as argument for power. In such a case, returns the result of self.sqrt()

--- dimensions/Surface.py
+++ dimensions/Surface.py
@@ -24,3 +24,21 @@ class Surface(AbstractQuantity):
         new_symbol = self.symbol.replace('2', '')
         new_quantity = sqrt(float(self))
         return Length(new_quantity, new_symbol)
+
+
+
+    def __pow__(self, power: int | float, modulo=None):
+        """
+        @param power            int | float             The exponent. ONLY INTEGERS OR 0.5 ARE SUPPORTED!
+        @param modulo           int                     The modulo
+
+        @return                 AbstractQuantity        The result of pow(@self, @exponent, @modulo)
+
+        @raise                  TypeError               if @exponent is neither an integer nor 0.5
+
+        If @power == 0.5, then return the result of self.sqrt().
+        Otherwise, calls the __pow__ method of AbstractQuantity
+        """
+        if power == 0.5:
+            return self.sqrt()
+        return super().__pow__(power, modulo)
