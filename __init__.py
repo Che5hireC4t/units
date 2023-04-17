7d70084a90d6d6036577b24a6308a42a28146e5e
Che5hireC4t <matthieu.epiard@protonmail.com>
2023-04-17 11:44:18 -0400

    Adding a version number to the package.

--- __init__.py
+++ __init__.py
@@ -1,5 +1,9 @@
 from sys import path
 
+# This is just a memo for myself in order to handle versioning properly:
+# https://betterprogramming.pub/why-versioning-is-important-and-how-to-do-it-686ce13b854f?gi=81d210140bbf
+__version__ = '1.0.0'
+
 __here_path = '/'.join(__file__.split('/')[:-1])
 path.append(__here_path) if __here_path not in path else None
 
