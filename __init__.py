42a77568c6b1e47bdb6b80069dfd35d07100bcff
Che5hireC4t <matthieu.epiard@protonmail.com>
2023-07-12 13:50:13 +0200

    Bugfix. The add of a new path in sys.path used "/" as directory separator, which is valid on Linux, but not on Windows.

--- __init__.py
+++ __init__.py
@@ -1,10 +1,11 @@
 from sys import path
+from os.path import dirname
 
 # This is just a memo for myself in order to handle versioning properly:
 # https://betterprogramming.pub/why-versioning-is-important-and-how-to-do-it-686ce13b854f?gi=81d210140bbf
-__version__ = '1.0.0'
+__version__ = '1.0.1'
 
-__here_path = '/'.join(__file__.split('/')[:-1])
+__here_path = dirname(__file__)
 path.append(__here_path) if __here_path not in path else None
 
 from .dimensions import *
