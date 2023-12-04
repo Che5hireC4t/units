f2b4cc1f63979eedecdaa3d6c0d1a82e5ed998d0
Che5hireC4t <matthieu.epiard@protonmail.com>
2023-12-04 17:24:44 -0500

    Adding basic tests for == operator.

---
+++ unittests/Test_Dimensions_Module/test_eq.py
@@ -0,0 +1,18 @@
+import pytest
+from dimensions import Mass
+
+
+TESTS = \
+    (
+        (Mass(1, 'g', None), Mass(1, 'g', None), True),
+        (Mass(1, 'g', 3), Mass(1, 'g', None), True),
+        (Mass(1, 'g', 3), Mass(1, 'g', 1), False),
+    )
+
+
+@pytest.mark.parametrize('test_data', TESTS)
+def test_eq(test_data) -> None:
+    value1, value2, expected_result = test_data
+    real_result = value1 == value2
+    assert real_result == expected_result
+    return
