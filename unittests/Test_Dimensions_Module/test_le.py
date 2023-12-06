2cb8a26a9180c7898f70e726f3bb89b54d5e7802
Che5hireC4t <matthieu.epiard@protonmail.com>
2023-12-06 10:37:11 -0500

    Adding basic tests for __le__ magic method.

---
+++ unittests/Test_Dimensions_Module/test_le.py
@@ -0,0 +1,21 @@
+import pytest
+from dimensions import Mass
+
+
+TESTS = \
+    (
+        (Mass(1, 'g', None), Mass(1, 'g', None), True),
+        (Mass(1, 'g', 3), Mass(1, 'g', None), True),
+        (Mass(1, 'g', 3), Mass(1, 'g', 1), False),
+        (Mass(1, 'g', 3), Mass(1, 'g', 3), True),
+        (Mass(1, 'g', 3), Mass(2, 'g', 3), True),
+        (Mass(56, 'g', 0), Mass(56.789, 'g', 3), False)
+    )
+
+
+@pytest.mark.parametrize('test_data', TESTS)
+def test_ne(test_data) -> None:
+    value1, value2, expected_result = test_data
+    real_result = value1 <= value2
+    assert real_result == expected_result
+    return
