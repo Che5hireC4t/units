dfb272d6f7fe2408ce3207beb69ff8a0f570eb26
Che5hireC4t <matthieu.epiard@protonmail.com>
2023-12-06 11:48:32 -0500

    Adding precision for molar masses.

--- constants/molar_masses.py
+++ constants/molar_masses.py
@@ -1,15 +1,15 @@
 from ..dimensions import MolarMass
 
-PROTON = MolarMass(1.0072765, 'g mol-1')
-NEUTRON = MolarMass(1.0086649, 'g mol-1')
-ELECTRON = MolarMass(0.0005486, 'g mol-1')
+PROTON = MolarMass(1.0072765, 'g mol-1', precision=7)
+NEUTRON = MolarMass(1.0086649, 'g mol-1', precision=7)
+ELECTRON = MolarMass(0.0005486, 'g mol-1', precision=7)
 
 # Atoms, sorted by weight
-HYDROGEN = MolarMass(1.0080, 'g mol-1')
-CARBON = MolarMass(12.011, 'g mol-1')
-NITROGEN = MolarMass(14.007, 'g mol-1')
-OXYGEN = MolarMass(15.999, 'g mol-1')
-SULFUR = MolarMass(32.066, 'g mol-1')
+HYDROGEN = MolarMass(1.0080, 'g mol-1', precision=4)
+CARBON = MolarMass(12.011, 'g mol-1', precision=3)
+NITROGEN = MolarMass(14.007, 'g mol-1', precision=3)
+OXYGEN = MolarMass(15.999, 'g mol-1', precision=3)
+SULFUR = MolarMass(32.066, 'g mol-1', precision=3)
 
 # Molecules, sorted in alphabetical order
 CARBON_DIOXIDE = CARBON + OXYGEN * 2
