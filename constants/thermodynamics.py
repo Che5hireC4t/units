4e8835f4480799f056c406d478d2ee050c5f29a4
Che5hireC4t <matthieu.epiard@protonmail.com>
2023-12-06 11:57:22 -0500

    Adding precision to WATER_BOILING_TEMPERATURE_IN_STANDARD_CONDITIONS

--- constants/thermodynamics.py
+++ constants/thermodynamics.py
@@ -1,4 +1,4 @@
 from ..dimensions import Temperature
 
 
-WATER_BOILING_TEMPERATURE_IN_STANDARD_CONDITIONS = Temperature(373.1339, 'K')
+WATER_BOILING_TEMPERATURE_IN_STANDARD_CONDITIONS = Temperature(373.1339, 'K', precision=4)
