f873131a5d84d8d107c38d2a6364fe80c8e3251c
Che5hireC4t <matthieu.epiard@protonmail.com>
2023-12-06 10:58:40 -0500

    Correction of the value of the gravitational constant.

--- constants/fundamental.py
+++ constants/fundamental.py
@@ -6,4 +6,4 @@ PLANCK_CONSTANT = Planck(6.62607015e-34, 'kg m2 sec-1', significant_digits=9)
 ELEMENTARY_CHARGE = ElectricCharge(1.602176634e-19, 'A sec', significant_digits=10)
 BOLTZMANN_CONSTANT = Entropy(1.380649e-23, 'J K-1', significant_digits=7)
 AVOGADRO_CONSTANT = ~SubstanceAmount(6.02214076e23, 'mol-1', significant_digits=9)  # https://en.wikipedia.org/wiki/Avogadro_constant
-GRAVITATIONAL_CONSTANT = Newton(6.6742e-11, 'm3 kg−1 sec−2', significant_digits=6)
+GRAVITATIONAL_CONSTANT = Newton(6.6743e-11, 'm3 kg−1 sec−2', significant_digits=5)
