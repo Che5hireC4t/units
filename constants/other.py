29bb7c3ab90fea8d15b24bc9024f4a6c40ffa1c8
Che5hireC4t <matthieu.epiard@protonmail.com>
2023-12-06 11:54:48 -0500

    adding precision for the delorean critical speed.

--- constants/other.py
+++ constants/other.py
@@ -1,3 +1,3 @@
 from ..dimensions import Speed
 
-DELOREAN_CRITICAL_SPEED = Speed(88, 'mph')  # https://en.wikipedia.org/wiki/DeLorean_time_machine
+DELOREAN_CRITICAL_SPEED = Speed(88, 'mph', precision=0)  # https://en.wikipedia.org/wiki/DeLorean_time_machine
