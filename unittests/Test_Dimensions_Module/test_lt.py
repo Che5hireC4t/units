68ed46de4122bad84c692d7148c2038545d97bc1
Che5hireC4t <matthieu.epiard@protonmail.com>
2023-12-06 10:27:43 -0500

    Adding a test for __lt__ magic method.

---
+++ unittests/Test_Dimensions_Module/test_lt.py
@@ -0,0 +1,20 @@
+import pytest
+from dimensions import Mass
+
+
+TESTS = \
+    (
+        (Mass(1, 'g', None), Mass(1, 'g', None), False),
+        (Mass(1, 'g', 3), Mass(1, 'g', None), False),
+        (Mass(1, 'g', 3), Mass(1, 'g', 1), False),
+        (Mass(1, 'g', 3), Mass(1, 'g', 3), False),
+        (Mass(1, 'g', 3), Mass(2, 'g', 3), True),
+    )
+
+
+@pytest.mark.parametrize('test_data', TESTS)
+def test_ne(test_data) -> None:
+    value1, value2, expected_result = test_data
+    real_result = value1 < value2
+    assert real_result == expected_result
+    return
