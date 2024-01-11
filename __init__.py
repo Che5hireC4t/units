a8d42a1abfd65d550c57654e4b071359f3e485d9
marmotte <matthieu.epiard@protonmail.com>
2024-01-11 23:59:53 +0000

    Updating to 1.1.0.
    Changelog:
    - Time class now allows to retrieve current unix timestamp into arbitrary time unit.

--- __init__.py
+++ __init__.py
@@ -3,7 +3,7 @@ from os.path import dirname
 
 # This is just a memo for myself in order to handle versioning properly:
 # https://betterprogramming.pub/why-versioning-is-important-and-how-to-do-it-686ce13b854f?gi=81d210140bbf
-__version__ = '1.0.6'
+__version__ = '1.1.0'
 
 __here_path = dirname(__file__)
 path.append(__here_path) if __here_path not in path else None
